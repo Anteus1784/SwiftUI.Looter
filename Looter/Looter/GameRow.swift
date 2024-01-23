cb4595c51ceb41d94d63518a29f76a859726066a
Antonio MICHEL <antonio.michel@etu.univ-smb.fr>
2024-01-23 11:56:56 +0100

    Finish app

---
+++ Looter/Looter/GameRow.swift
@@ -0,0 +1,8 @@
+//
+//  GameRow.swift
+//  Looter
+//
+//  Created by Antonio MICHEL on 1/23/24.
+//
+
+import Foundation
---
+++ Looter/Looter/LootRow.swift
@@ -0,0 +1,8 @@
+//
+//  LootRow.swift
+//  Looter
+//
+//  Created by Antonio MICHEL on 1/23/24.
+//
+
+import Foundation
